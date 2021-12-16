edcdabd88e4e6bf9e3a3f06abe0e6b040ee69010
menachem554 <mendyrivkin@gmail.com>
2021-12-16 18:59:15 +0200

    m

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,12 +1,15 @@
 <template>
-    <v-app>
-        <v-card width="400" class="mx-auto mt-5">
-            <v-card-title>
-                <h1 class="display-1">Login</h1>
-            </v-card-title>
-            <v-card-text>
-                <v-form>
-                    <v-text-field
+  <v-card
+    class="elevation-10
+    grey lighten-5"
+    max-width="400"
+  >
+    <v-card-title>
+         <h1 class="display-1">Login</h1>
+    </v-card-title> 
+        <v-card-text>
+            <v-form>
+                <v-text-field
                     label="Username"
                     prepend-icon="mdi-account-circle"
                     />
@@ -23,12 +26,11 @@
             </v-card-text>
             <v-divider></v-divider>
             <v-card-actions>
-                <v-btn color="success">Register</v-btn>
+                <v-btn rounded class="green lighten-1 white--text">Register</v-btn>
                 <v-spacer></v-spacer>
-                <v-btn color="info">Login</v-btn>
+                <v-btn rounded class="light-blue lighten-1 white--text">Login</v-btn>
             </v-card-actions>
         </v-card>
-    </v-app>
 </template>
 
 <script>
---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <TopNav />
+      <Login />
+      <Footer />
+    </div>
+</template>
+
+<script>
+import TopNav from '../components/TopNav.vue'
+import Login from '../components/home/PopularProducts.vue'
+import Footer from '../components/home/Footer.vue'
+
+export default {
+  components: {
+    Footer,
+    TopNav,
+    Login
+  }
+}
+</script>
